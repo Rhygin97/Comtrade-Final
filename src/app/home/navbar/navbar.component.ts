da8e62801d9ebfc44742b647106671a1b87144ce
Rhygin97 <skillstopaydabills@gmail.com>
2020-06-14 17:58:19 +0200

    navbar attempt

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { faIdCard } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +11,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  faUser = faUser;
+  faPhone = faPhone;
+  faLock = faLock;
+  faIdCard = faIdCard;
+  faSearch = faSearch;
   constructor() { }
 
   ngOnInit(): void {
